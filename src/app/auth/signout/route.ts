fd3c3c9e8bbebab6847f34de44c175957637819b
Ahmedkamal7 <83322640+Ahmedkamal7@users.noreply.github.com>
2026-07-24 10:46:32 +0300

    Implement sign-out route for user authentication

---
+++ src/app/auth/signout/route.ts
@@ -0,0 +1,12 @@
+import { createClient } from "@/lib/supabase/server";
+import { revalidatePath } from "next/cache";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const supabase = createClient();
+  await supabase.auth.signOut();
+  revalidatePath("/", "layout");
+  return NextResponse.redirect(new URL("/login", req.url), {
+    status: 302,
+  });
+}
